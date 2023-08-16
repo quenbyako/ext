f14f19980cbffaeb9d9866ec7bb6dd269e8e5f73
Richard Cooper <rcooper.xelaj@protonmail.com>
2023-08-17 00:13:01 +0200

    added contains function

--- maps/maps.go
+++ maps/maps.go
@@ -138,3 +138,18 @@ func Remap[M1 ~map[K1]V1, K1, K2 comparable, V1, V2 any](m M1, f func(K1, V1) (K
 
 	return res
 }
+
+func Contains[M ~map[K]V, K, V comparable](m M, k K, v V) bool {
+	got, ok := m[k]
+	return ok && got == v
+}
+
+func ContainsFunc[M ~map[K]V, K comparable, V any](m M, f func(K, V) bool) bool {
+	for k, v := range m {
+		if f(k, v) {
+			return true
+		}
+	}
+
+	return false
+}
